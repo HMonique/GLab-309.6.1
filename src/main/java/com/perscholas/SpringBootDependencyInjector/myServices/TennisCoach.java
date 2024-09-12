e3277d10c4c4c6b37c3e49e51613a5ef8cfe841e
HMonique <moniquehl@outlook.com>
2024-09-12 14:44:59 -0400

    third commit

--- src/main/java/com/perscholas/SpringBootDependencyInjector/myServices/TennisCoach.java
+++ src/main/java/com/perscholas/SpringBootDependencyInjector/myServices/TennisCoach.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
     @Autowired
-    @Qualifier("randomFortuneService")
+    @Qualifier("databaseFortuneService")
     private FortuneServices fortuneServices;
 
     private FortuneServices fortuneService;
